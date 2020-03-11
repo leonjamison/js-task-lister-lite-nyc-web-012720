9c3c85202fec700a385c97eaff3177f7510ba68a
Leon Jamison <leonjamison@icloud.com>
2020-03-11 09:17:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  
+  let input =  document.getElementById('new-task-description')
+  let ul = document.getElementsByTagName('ul')[0]
+  
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    let target = event.target 
+    let div = target.parentNode.querySelector('div')
+    let li = document.createElement('li')
+ 
+    li.innerText = input.value 
+
+    ul.append(li)
+
+    //  li.innerHTML = input.value 
+    //  ul.append(li)
+  
+})
+ 
+ 
+
 });
